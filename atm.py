f8c2954d8687ab00e1671ef86159adb504ba6764
Hamdi Ben barka <hmdibenbarka@gmail.com>
2018-09-14 14:49:20 +0100

    improve code by adding a list of allowed paper

--- atm.py
+++ atm.py
@@ -1,15 +1,12 @@
-def atm(request) :
-	while request >= 100:
-		request -=100
-		print "give 100$"
-	while request >= 50:
-		request -=50
-		print "give 50$"
-	while request >= 10:
-		request -=10
-		print "give 10$"
-	while request >= 5:
-		request -=5
-		print "give 5$"
-	if request != 0 :
-		print "give " + str(request) + "$"
+def atm(request):
+    request = int(request)
+    allowed_paper = [200, 100, 50, 10, 5]
+    for i in range(len(allowed_paper)):
+        while request >= allowed_paper[i]:
+            request -= allowed_paper[i]
+            print "give " + str(allowed_paper[i]) + "$"
+        if request < allowed_paper[-1]:
+            print "give " + str(request) + "$"
+
+
+atm(raw_input("enter the amount you want to withdraw: "))
